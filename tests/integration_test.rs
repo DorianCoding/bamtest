5fa547adf48bfa95d2b3fbc4f3d095cd8423c1cd
Timofey Prodanov <timofey.prodanov@gmail.com>
2020-01-21 23:46:04 -0800

    Add two tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -192,6 +192,81 @@ fn test_bam_to_bam(path: &str, additional_threads: u16) {
     compare_sam_files(&output1, &output2);
 }
 
+fn test_ind_bam_to_bam(path: &str, additional_threads: u16) {
+    let mut reader = bam::IndexedReader::build()
+        .additional_threads(additional_threads)
+        .from_path(path).unwrap();
+    let header = reader.header().clone();
+
+    const ITERATIONS: usize = 100;
+    let mut rng = rand::thread_rng();
+    let mut record = bam::Record::new();
+
+    let bam_output = format!("tests/data/tmp/bamcrate.ind_bam_to_bam_t{}.bam", additional_threads);
+    let output1 = format!("tests/data/tmp/bamcrate.ind_bam_to_bam_t{}.sam", additional_threads);
+    let output2 = format!("tests/data/tmp/samtools.ind_bam_to_bam_t{}.sam", additional_threads);
+    let mut non_empty = 0;
+    for i in 0..10 * ITERATIONS {
+        if non_empty == ITERATIONS {
+            break;
+        }
+        let ref_id = rng.gen_range(0, header.n_references());
+        let length = header.reference_len(ref_id as u32).unwrap();
+        let start = rng.gen_range(0, length);
+        let end = rng.gen_range(start + 1, length + 1);
+
+        let mut count = 0;
+        let ref_name = header.reference_name(ref_id as u32).unwrap();
+        println!("    Iteration {}", i);
+        println!("    Fetching {}:{}-{}", ref_name, start + 1, end);
+
+        let timer = Instant::now();
+        let mut bam_writer = bam::BamWriter::build()
+            .additional_threads(additional_threads)
+            .from_path(&bam_output, reader.header().clone())
+            .unwrap();
+        let mut viewer = reader.fetch(&bam::Region::new(ref_id as u32, start, end)).unwrap();
+        loop {
+            match viewer.read_into(&mut record) {
+                Ok(true) => {},
+                Ok(false) => break,
+                Err(e) => panic!("{}", e),
+            }
+            count += 1;
+            bam_writer.write(&record).unwrap();
+        }
+        bam_writer.finish().unwrap();
+        println!("        bam::IndexedReader: {:?}", timer.elapsed());
+
+        let timer = Instant::now();
+        let mut child = Command::new("samtools")
+            .args(&["view", "-h"])
+            .arg(&path)
+            .arg(format!("{}:{}-{}", ref_name, start + 1, end))
+            .args(&["-o", &output2])
+            .spawn()
+            .expect("Failed to run samtools view");
+        let ecode = child.wait().expect("Failed to wait on samtools view");
+        assert!(ecode.success());
+        println!("        samtools view:  {:?}", timer.elapsed());
+        println!("        total {} records", count);
+
+        let mut child = Command::new("samtools")
+        .args(&["view", "-h"])
+        .arg(&bam_output)
+        .args(&["-o", &output1])
+        .spawn()
+        .expect("Failed to run samtools view");
+        let ecode = child.wait().expect("Failed to wait on samtools view");
+        assert!(ecode.success());
+
+        compare_sam_files(&output1, &output2);
+        if count > 0 {
+            non_empty += 1;
+        }
+    }
+}
+
 fn test_sam_to_bam(path: &str) {
     let mut reader = bam::SamReader::from_path(path).unwrap();
     let mut record = bam::Record::new();
@@ -305,5 +380,23 @@ fn bam_to_bam_multithread() {
         println!("Analyzing {}", entry.display());
         let entry_str = entry.as_os_str().to_str().unwrap();
         test_bam_to_bam(entry_str, 2);
+    }
+}
+
+#[test]
+fn ind_bam_to_bam_singlethread() {
+    for entry in indexed_bam_files() {
+        println!("Analyzing {}", entry.display());
+        let entry_str = entry.as_os_str().to_str().unwrap();
+        test_ind_bam_to_bam(entry_str, 0);
+    }
+}
+
+#[test]
+fn ind_bam_to_bam_multithread() {
+    for entry in indexed_bam_files() {
+        println!("Analyzing {}", entry.display());
+        let entry_str = entry.as_os_str().to_str().unwrap();
+        test_ind_bam_to_bam(entry_str, 2);
     }
 }
